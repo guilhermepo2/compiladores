e5f81c6bd04b8cd2bd47962f25d4b79942c93e73
Guilherme Oliveira <guilherme.061@gmail.com>
2016-08-13 23:40:53 -0300

    Começando Relatório
    upando todos os códigos lua de novo, não parece muito certo né

---
+++ relatorio/codigos/lua/micro10.lua
@@ -0,0 +1,16 @@
+--Listagem 22: Calcula o fatorial de um numero
+
+--[[ Função: recebe um número e calcula recursivamente o fatorial desse número --]]
+
+function fatorial(n)
+	 if(n <= 0)
+	 then return 1
+	 else return (n* fatorial(n-1))
+	 end
+end
+
+print("Digite um numero: ")
+numero = tonumber(io.read())
+fat = fatorial(numero)
+
+print("O fatorial de", numero, "e: ", fat)
