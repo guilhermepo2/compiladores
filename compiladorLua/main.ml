7ebae0f02905515c7f30a86ed71882f47c4db2bb
Guilherme Oliveira <guilherme.061@gmail.com>
2016-11-07 15:55:33 -0200

    vai filhao

--- compiladorLua/main.ml
+++ compiladorLua/main.ml
@@ -1,6 +1,7 @@
 let parse_arq arq =
   let ic = open_in arq in
   let lexbuf = Lexing.from_channel ic in
-  let _ = Parser.chunk Lualexer.token lexbuf in
-  print_endline "Ok"
+  let ast = Parser.chunk Lualexer.token lexbuf in
+  let _ = close_in ic in
+  ast
 ;;
